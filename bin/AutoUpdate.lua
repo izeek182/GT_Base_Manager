2824c03e52efd6a404ed5bdaddf7f7e7dee70719
izeek182 <izeek182@gmail.com>
2023-03-22 13:17:32 -0600

    Init Added file config

---
+++ bin/AutoUpdate.lua
@@ -0,0 +1,17 @@
+
+-- Local Files --------------------------
+--      /cgf/NetworkVersion
+--          table{installedprograms={},program={version,clients}}
+--      /cgf/programDepenancies
+--          table{program = {depenantCnt=int(number of progams depenant on this program), dependees={Programs that this program dependends on}}}
+
+
+-- Sudo Code --------------------------
+-- Request Repo Version Information,
+-- Read localhosts Used Versions hosts
+-- cross referance ve3rsions in repo
+-- pull updated libs + program files,
+-- Distribute to the clients on network
+-- On confirmation, record new version number locally
+
+
