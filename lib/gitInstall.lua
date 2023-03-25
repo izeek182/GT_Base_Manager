5c1e5325a492e8815a35a291e0317a6a0960f789
izeek182 <izeek182@gmail.com>
2023-03-25 13:34:43 -0600

    changed cfg format slightly+added functionality to gitInstall

--- lib/gitInstall.lua
+++ lib/gitInstall.lua
@@ -1,42 +1,75 @@
 local gitInstall = {}
 local net = require("internet")
 local s = require("serialization")
-local tf = require("tableToFile")
-gitInstall.ProgramsTable = {}
+local ft = require("tableToFile")
+local localVersionFile = "/etc/gitVersion."
 
-
-
-local function getWebTable(url)
+local function downloadPage(url)
     local a = net.request(url)
-    a.read()
-    local file = a.read()
-    return s.unserialize(file)
+    local s = ""
+    for c in a do
+        s = s .. a
+    end
 end
 
-local function getWebFile(url,location)
-    local a = net.request(url)
-    a.read()
-    local file = a.read()
-    local tableFile = assert(io.open(location, "w"))
-    tableFile:write(file)
+local function getWebTable(url)
+    local f = downloadPage(url)
+    return s.unserialize(f)
+end
+
+local function getWebFile(url,dest)
+    local f = downloadPage(url)
+    local tableFile = assert(io.open(dest, "w"))
+    tableFile:write(f)
     tableFile:close()
 end
 
-function gitInstall.getOptions(url)
-    gitInstall.ProgramsTable = getWebTable(url)
+function gitInstall:getVersion(fileName)
+    if(self.locVer == nil)then
+        self.locVer = ft.load(localVersionFile)
+    end
+    if(self.locVer[fileName] ~= nil)then
+        return self.locVer[fileName]
+    else
+        return 0
+    end
 end
 
-function gitInstall.install(name)
-    gitInstall.getOptions("https://raw.githubusercontent.com/izeek182/GT_Base_Manager/main/programs.cfg")
-    if (gitInstall.ProgramsTable[name] ~= nil) then 
-        local p = gitInstall.ProgramsTable[name]
-        local url = "https://"..gitInstall.ProgramsTable.githubLink
+function gitInstall:logVersion(fileName,version)
+    if(self.locVer == nil)then
+        self.locVer = ft.load(localVersionFile)
+    end
+    self.locVer[fileName] = version
+end
+
+function gitInstall:getOptions(url)
+    self.opts = getWebTable(url)
+end
+
+function gitInstall:install(name)
+    if (self.opts[name] ~= nil) then 
+        local p = self.opts[name]
+        local url = "https://"..self.opts.githubLink
         local cfg = getWebTable(url..p.installConfig)
-        for index, file in pairs(cfg.bin) do
-            getWebFile(url..file,"bin/"..file)
-        end
-        for index, file in pairs(cfg.lib) do
-            getWebFile(url..file,"lib/"..file)
+        for index, file in pairs(cfg.files) do
+            local v = cfg.fileVersion[file]
+            if (self:getVersion(file) < v) then
+                print("\"" .. file .. "\" is out of date, updating to V"..v)
+                getWebFile(url..file,"/bin/"..file)
+                self:logVersion(file,v)
+            else 
+                print("\"" .. file .. "\" up to date skipping")
+            end
         end
     end
 end
+
+local function init()
+    gitInstall:getVersion()
+    gitInstall.opts = {}
+    gitInstall:getOptions("https://raw.githubusercontent.com/izeek182/GT_Base_Manager/main/programs.cfg")
+end
+
+init()
+
+return gitInstall
