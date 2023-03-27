e0d4c60b8134faf768189cd7fd6977dc7cc4fd38
izeek182 <izeek182@gmail.com>
2023-03-26 21:10:01 -0600

    Started provider, subscriber model

---
+++ NetDoc.md
@@ -0,0 +1,29 @@
+Ports
+
+| Port | Description |
+| --- | ----------- |
+| 8008 | Logger broadcast |
+| 20   | Discovery port |
+| 21   | ping port |
+| 25   | HeartBeat port |
+| 30 | Component port |
+| 35 | GLasses port |
+| 40 | FTP |
+
+ServiceCode
+
+| Enum | Description |
+| ---  | ----------- |
+| 2    | PassUp      |
+| 3    | ADP         |
+| 4    | Debug Net   |
+
+Subscription Types:
+| Description       | Tag       |
+| -----------       | ---       |
+| GT Machine Gen    | GT_Gen    |
+| GT Power          | GT_Pow    |
+| AE Items          | AE_I      |
+| AE Fluid          | AE_F      |
+| Source Control    | OC_src    |
+
---
+++ lib/netDefs.lua
@@ -0,0 +1,36 @@
+if(_NetDefs == nil) then 
+    _NetDefs = {}
+    _NetDefs.loggerEnum = {
+        error = 0,
+        info  = 1,
+        toString = function(num)
+            if num == _NetDefs.loggerEnum.error then
+                return "Error"
+            end
+            if num == _NetDefs.loggerEnum.info then
+                return "Info "
+            end
+        end
+    }
+    _NetDefs.hbEnum = {
+        live    = 0,
+        waiting = 1,
+        overdue = 2
+    }
+    _NetDefs.remoteEnum = {
+        server      = 0,
+        componant   = 1,
+        display     = 2,
+        terminal    = 3
+    }
+    _NetDefs.portEnum = {
+        logger          = 8008,
+        adp             = 20,
+        ping            = 21,
+        heartBeat       = 25,
+        componantCmd    = 30,
+        FTP             = 40
+    }
+    _NetDefs.START = 0
+    _NetDefs.END   = 420
+end
