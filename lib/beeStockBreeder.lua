7d9e281d4272eff85d65fb800aafbdf4f9559922
izeek182 <izeek182@gmail.com>
2024-04-17 17:35:10 -0600

    polish

--- lib/beeStockBreeder.lua
+++ lib/beeStockBreeder.lua
@@ -2,13 +2,5 @@
 ---@module "components"
 
 ---@class BeeBreeder
----@field IsPureBreed fun(bee:BeeStack):integer Returns A integer representing the quality of a Bee  
----@field remoteData fun(if:Interface,localAddress:string, remoteAddress:string, port:number, distance:number, l1Header:string, data):boolean
----@field broadcast fun(if:Interface,port:number, ...):boolean
----@field open fun(if:Interface,port:number):boolean
----@field close fun(if:Interface,port:number):boolean
----@field getPortStatus fun(if:Interface,port:number):boolean
----@field isWireless fun(if:Interface):boolean
-local Interface = {status = false}
-
-
+---@field IsPureBreed fun(bee:BeeStack):integer Returns A integer representing the quality of a Bee
+local Interface = { status = false }
